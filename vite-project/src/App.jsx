e156888f7b0d6c2b9871688e60485b0ae7b2ac3a
Diego Peralta <peralta.d2311@gmail.com>
2026-08-03 18:35:56 -0300

    add: app.jsx variables

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import Header from "./components/Header.jsx";
+import { WriteArea } from "./components/WriteArea.jsx";
+import { Controlls } from "./components/Controlls.jsx";
+import { Stats } from "./components/Stats.jsx";
+import { LetterDensity } from "./components/LetterDensity.jsx";
+import "./App.css";
+
+// ========================================================
+
+const App = () => {
+    const [text, setText] = useState("")
+
+    const [excludeSpaces, setExcludeSpaces] = useState(false)
+    const [limitCharacter, setLimitCharacter] = useState(false)
+    const [limitValue, setLimitValue] = useState(300)
+    const [showAll, setShowAll] = useState(false)
+    const [darkMode, setDarkMode] = useState(true)
+}
+
+
+// ========================================================
+
+const handleExcludeSpaces = () => {
+    setExcludeSpaces(!excludeSpaces)
+}
+
+const handleLimitValue = (Value) => {
+    setLimitValue(Number(value))
+}
+
+const characters = excludeSpaces ? text.replace(/\s/g, "").length : text.length
+
+const words =
+    text.trim() === "" ? 0 : text.trim().split(/\s+/).length
+
+const sentences =
+    text.trim() === "" ? 0 :
+        text.split(/[.!?]+/).filter(sentence => sentence.trim() !== "").length
+
+const readingTime = words === 0
+    ? "< 1 minuto"
+    : `${Math.ceil(words / 200)} minuto`;
+
+const handleTheme = () => {
+    setDarkMode(!darkMode)
+}
+
+const handleChangeTextarea = (e) => {
+    const value = e.target.value
+
+    if (limitCharacter && value.length > limitValue)
+        setText(value)
+}
+
+const handleChangeInputLimit = () => {
+    setLimitCharacter(!limitCharacter)
+    const newText = text.slice(0, limitValue)
+    setText(newText)
+}
+
+const cleanText = text.toLowerCase().replace(/[^a-záéíóúñü]/g, "")
+const total = cleanText.length
+
+const dictionaryLetters = {}
+
+cleanText.split("").forEach(letter => {
+    dictionaryLetters[letter] = (dictionaryLetters[letter] || 0) + 1
+})
+
+const letters = Object.entries(dictionaryLetters).map(dataLetter => {
+    const letter = dataLetter[0]
+    const amountValue = dataLetter[1]
+
+    const infoToRenderLetter = {
+        letterName: letter,
+        amount: amountValue,
+        percentage: (amountValue / total) * 100
+    }
+    return infoToRenderLetter
+})
+
+
+const sortLetters = letters.sort((a, b) => b.amount - a.amount)
+
+const visibleLetters = showAll ? sortLetters : sortLetters.slice(0, 5)
+
+
+// ========================================================
